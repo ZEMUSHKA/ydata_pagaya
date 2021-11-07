6649a21805204a301a890279c3c58d656b58cb08
Andrey <zimovnov@gmail.com>
2021-11-07 14:33:50 +0300

    v1

--- bootstrap.sh
+++ bootstrap.sh
@@ -38,8 +38,8 @@ then
   sudo /usr/local/bin/jupyter nbextension enable --py nbdime --system
   echo "jupyter notebook --no-browser" > /home/hadoop/start_jupyter.sh
   sudo chmod +x /home/hadoop/start_jupyter.sh
-  # git clone https://github.com/ZEMUSHKA/ydata_lsml.git /home/hadoop/ydata_lsml
-  # sudo cp /home/hadoop/ydata_lsml/libboost_program_options.so.1.73.0 /usr/lib64/
+  git clone https://github.com/ZEMUSHKA/ydata_pagaya.git /home/hadoop/ydata_pagaya
+  # sudo cp /home/hadoop/ydata_pagaya/libboost_program_options.so.1.73.0 /usr/lib64/
 fi
 
 sudo pip-3.6 install sklearn numpy scipy pandas matplotlib ipywidgets tqdm
